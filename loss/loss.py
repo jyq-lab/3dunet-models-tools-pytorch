729e19f8cbb20bc81195d57fae70c63dd46337a1
Geralt <73121494+jyq-lab@users.noreply.github.com>
2024-05-28 10:01:08 +0800

    Update loss.py

--- loss/loss.py
+++ loss/loss.py
@@ -1,3 +1,16 @@
+def _dice_loss(predict, target, pow=False, alpha=1):
+    smooth = 1e-5
+
+    intersection = torch.sum(predict * target)
+    if pow:
+        predict = torch.pow(predict, 2)
+        target = torch.pow(target, 2)
+
+    union = torch.sum(predict) + torch.sum(target) * alpha 
+    _dice = ( ((1 + alpha) * intersection) + smooth ) / ( union + smooth )
+    return 1 - _dice
+
+
 class MultiDiceLossW(nn.Module):
     ''' multi-class dice loss with weight. '''
     def __init__(self, 
@@ -18,7 +31,7 @@ class MultiDiceLossW(nn.Module):
         
         outs = logit
         if self.activation == True:
-            outs = torch.sigmoid(outs) if cls == 1 else F.softmax(outs, dim=1)
+            outs = torch.sigmoid(outs) if cls == 1 else torch.softmax(outs, dim=1)
 
         if self.include_background == False:
             if cls == 1:
@@ -30,41 +43,13 @@ class MultiDiceLossW(nn.Module):
         labels = labels.float()
 
         self.weight = [1.0] * cls if self.weight is None else self.weight
+        assert len(self.weight) == cls, " weight must correspond to category(include background) "
 
         total_loss = 0
         for i in range(cls):
-            _i_dice_loss = _dice_loss(outs[:, i], labels[:, i], self.squared_pred, 1)
+            _i_dice_loss = _dice_loss(outs[:, i], labels[:, i], self.squared_pred, 10)
+            print("{:.4f}".format(_i_dice_loss.item()), end=' ')
+
             _i_w_dice_loss = _i_dice_loss * self.weight[i]
             total_loss += _i_w_dice_loss
         return total_loss / cls
-
-
-class DiceCELoss(nn.Module):
-    def __init__(self, 
-                 include_background, 
-                 squared_pred,
-                 activation=False, 
-                 lambda_dice: float = 1.0,
-                 lambda_ce: float = 1.0,
-                 ):
-        super(DiceCELoss, self).__init__()
-        assert  0.0 <= lambda_dice <= 1.0, \
-            "lambda_dice should not be less than 0.0 and should not be greater than 1.0"
-        assert  0.0 <= lambda_ce <= 1.0, \
-            "lambda_dice should not be less than 0.0 and should not be greater than 1.0"
-        self.lambda_dice, self.lambda_ce = lambda_dice, lambda_ce
-
-        self.dice = MultiDiceLossW(
-            weight=None, 
-            activation=False,
-            include_background=include_background,
-            squared_pred=squared_pred)
-        
-        # one-hot data must be processed with softmax
-        self.ce = nn.CrossEntropyLoss()
-
-    def forward(self, input: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
-        dice_loss = self.dice(input, target)
-        ce_loss = self.ce(input, target)
-        total_loss: torch.Tensor = self.lambda_dice * dice_loss + self.lambda_ce * ce_loss
-        return total_loss
