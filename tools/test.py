f230cc32f263a024a068e10f25cb5fbc931c98b2
Geralt <73121494+jyq-lab@users.noreply.github.com>
2025-01-26 18:03:15 +0800

    Update test.py

--- tools/test.py
+++ tools/test.py
@@ -13,3 +13,20 @@ def euclidean_distance(p1, p2):
     z1, y1, x1 = p1
     z2, y2, x2 = p2
     return math.sqrt((z2 - z1) ** 2 + (y2 - y1) ** 2 + (x2 - x1) ** 2)
+
+def calculate(pred, true):
+    y_pred = pred.flatten()
+    y_true = true.flatten()
+    cm = confusion_matrix(y_pred, y_true)
+    TP = cm[1, 1]
+    TN = cm[0, 0]
+    FP = cm[0, 1]
+    FN = cm[1, 0]
+    accuracy = (TP + TN) / (TP + TN + FP + FN)
+    precision = TP / (TP + FP)
+    specificity = TN / (TN + FP)
+    recall = TP / (TP + FN)
+    iou = TP / (FP + TP + FN)
+    dice = 2*TP / (FP + 2*TP + FN)
+    
+    return accuracy, precision, specificity, recall, iou, dice
